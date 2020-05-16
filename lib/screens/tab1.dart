5560a35a6fdf3dd534d191fb174746a5ccd7732e
kleviss <klevissxh@gmail.com>
2020-05-16 09:39:12 +0200

    Tab 1 initalUrl change

--- lib/screens/tab1.dart
+++ lib/screens/tab1.dart
@@ -31,7 +31,7 @@ class _Tab1State extends State<Tab1> with AutomaticKeepAliveClientMixin{
         child: Stack(
           children: <Widget>[
             WebView(
-              initialUrl: 'https://qytetarin.com/',
+              initialUrl: 'https://flutter.dev/',
               javascriptMode: JavascriptMode.unrestricted,
               onWebViewCreated: (WebViewController webViewController) {
                 _controller.complete(webViewController);
